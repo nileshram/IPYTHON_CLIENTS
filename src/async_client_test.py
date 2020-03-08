f46541e6afb8d82d322a5c10e84e9e3a03db6f42
nilesh <nilesh@nileshdev>
2020-03-08 16:47:35 +0000

    code examples of async and normal clients

--- src/async_client_test.py
+++ src/async_client_test.py
@@ -1,22 +1,21 @@
 '''
-Created on 4 Mar 2020
+Created on 8 Mar 2020
 
 @author: nilesh
 '''
+import asyncio
+from jupyter_client.asynchronous.client import AsyncKernelClient
 
-from jupyter_client.blocking.client import BlockingKernelClient
-
-    
-class CustomJupyterKernelClient(BlockingKernelClient):
+class CustomJupyterKernelClient(AsyncKernelClient):
     
     def __init__(self):
         super(CustomJupyterKernelClient, self).__init__()
-        self._init_connection(connection_file="/home/nilesh/.local/share/jupyter/runtime/kernel-30840.json")
+        self._init_connection(connection_file="/home/nilesh/.local/share/jupyter/runtime/kernel-32350.json")
     
     def _init_connection(self, connection_file=None):
         self.load_connection_file(connection_file)
 
-    def compute_option_pricing(self, code, silent=False, store_history=True,
+    async def compute_option_pricing(self, code, silent=False, store_history=True,
                 user_expressions=None, allow_stdin=None, stop_on_error=True):
 
         content = dict(code=code, silent=silent, store_history=store_history,
@@ -27,15 +26,13 @@ class CustomJupyterKernelClient(BlockingKernelClient):
         self.shell_channel.send(msg)
         return msg['header']['msg_id']
 
+async def main():
+    kc = CustomJupyterKernelClient()
+    while True:
+        res = await kc.compute_option_pricing("2+2")
+        msg = await kc.get_iopub_msg()
+        print(msg)
 
-
-Test = CustomJupyterKernelClient()
-msg_id = Test.compute_option_pricing("2+2")
-io_msg = Test.get_iopub_msg(msg_id)
-print(Test.get_iopub_msg())
-
-
-    
-    
-    
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
     
